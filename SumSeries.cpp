695d89e5100afa5447edd5a0a8da27aa8c3f1736
PotatoMine725 <WotblitzGamer725@gmail.com>
2024-10-14 13:03:34 +0700

    SUM

---
+++ SumSeries.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int Cal (int *ds,int n) {
+    int sum = 0;
+    for ( i = 0; i < n; i++)
+    {
+        sum = sum + (ds[i]*pow(x,i))
+    }
+    return sum;
+}
+
+void nhap(int *ds, int spt) {
+    for (int i = 0; i < spt; i++)
+    {
+        printf("Nhap phan tu thu %d", i+1);
+    }
+}
+
+int main()
+{
+    int *ds,spt;
+    printf("Nhap so luong phan tu mang: ");
+    scanf("%d",&spt);
+    ds = new int[spt];
+}
+
+
